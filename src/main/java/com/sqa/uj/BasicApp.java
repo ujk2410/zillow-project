3b4fb2ba8d2d129e9971b116f8637009e6d49cc6
Uj Kaur <ujk2410@gmail.com>
2017-03-22 20:01:41 -0700

    C:/Users/Uj/Dropbox/Workspace/ebay-project/drivers

---
+++ src/main/java/com/sqa/uj/BasicApp.java
@@ -0,0 +1,35 @@
+/**
+ * File Name: BasicApp.java<br>
+ * Kaur, UJ<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Mar 4, 2017
+ */
+package com.sqa.uj;
+
+/**
+ * BasicApp //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Kaur, UJ
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class BasicApp {
+
+	/**
+	 * core method to run as application
+	 * 
+	 * @param args
+	 *            Supplied arguments to main method
+	 */
+	public static void main(String[] args) {
+		System.out.print("┏(-_-)┛\n");
+		System.out.print("┏(-_-)┛\n");
+		System.out.print("┏(-_-)┛\n");
+		System.out.print("┏(-_-)┓");
+	}
+}
